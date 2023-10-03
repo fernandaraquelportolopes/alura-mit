a333d6a02fd02400e32051c95108f71eccfdb1d5
fernandaraquelportolopes <129179318+fernandaraquelportolopes@users.noreply.github.com>
2023-10-03 08:58:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ function tocaSomPom(){
     document.querySelector('som_tecla_pom').play('');   
 }
 
-document.querySelector('.tecla_pom'). onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla')
 
+ListaDeTeclas[0].onclick = tocaSomPom;
+
+ListaDeTeclas[1].onclick = tocaSomClap;
+ListaDeTeclas[2].onclick = tocaSomTim;
+ListaDeTeclas[3].onclick = tocaSomPuff;
