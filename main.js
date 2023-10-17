fc056314951384c5685ee55f8dce1c8881d40546
fernandaraquelportolopes <129179318+fernandaraquelportolopes@users.noreply.github.com>
2023-10-17 08:30:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-function tocaSomPom(){
-    document.querySelector('som_tecla_pom').play('');   
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play('');   
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla')
 
-ListaDeTeclas[0].onclick = tocaSomPom;
+
+
+for (contador = 0; contador < listaDeTecla.length; contador++){
+    constInstrumento = ListaDeTeclas[contador].clssList[1];
+    const tecla = ListaDeTeclas[contador];
+
+    const idAudio = '#som_${instrumento}';
+    tecla[contador].onclick = function(){
+        tocaSom('#som_tecla_clap')
+    };
+}
+
 
 ListaDeTeclas[1].onclick = tocaSomClap;
 ListaDeTeclas[2].onclick = tocaSomTim;
